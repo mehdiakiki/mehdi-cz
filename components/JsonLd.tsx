68b822f5683582dc008ed834898f352a383c6766
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2026-02-13 20:53:55 -0500

    cleanup:remove unused imports, variables, and commented code

--- components/JsonLd.tsx
+++ components/JsonLd.tsx
@@ -1,4 +1,3 @@
-import { Metadata } from "next";
 import siteMetadata from "@/data/siteMetadata";
 
 interface JsonLdProps {
--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -25,7 +25,7 @@ interface LayoutProps {
 }
 
 export default function PostLayout({ content, authorDetails, next, prev, children }: LayoutProps) {
-  const { filePath, path, slug, date, title, tags } = content;
+  const { path, date, title, tags } = content;
   const basePath = path.split("/")[0];
 
   return (
@@ -92,14 +92,6 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
             </dl>
             <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
               <div className="prose max-w-none pb-8 pt-10 dark:prose-invert">{children}</div>
-              {/*{siteMetadata.comments && (
-                <div
-                  className="pb-6 pt-6 text-center text-gray-700 dark:text-gray-300"
-                  id="comment"
-                >
-                  <Comments slug={slug} />
-                </div>
-              )}*/}
             </div>
             <footer>
               <div className="divide-gray-200 text-sm font-medium leading-5 dark:divide-gray-700 xl:col-start-1 xl:row-start-2 xl:divide-y">
